384d47a89327526522933e9a5d6c65c467b9cbe6
Liangyue "Yue" Zhao <Hello56721@github.com>
2021-11-16 12:21:15 -0500

    Wrote a little bit of the command line option processer

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -2,8 +2,82 @@
 #include <sstream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <stdexcept>
+
+Options processCommandLineArguments(std::vector<std::string> arguments);
+
+enum Mode
+{
+    NENGISH_TO_TEXT, TEXT_TO_NENGISH
+};
+
+struct Options
+{
+    Mode mode;
+
+    std::string inputFileName;
+    std::string outputFileName;
+};
+
+struct InvalidUsageException
+{
+    
+};
+
+int myMain(int argc, char** argv)
+{
+    std::vector<std::string> commandLineArguments;
+    for (int i = 0; i < argc; i++)
+    {
+        commandLineArguments.push_back(argv[i]);
+    }
+    
+    Options options = processCommandLineArguments(commandLineArguments);
+    
+    return 0;
+}
 
 int main(int argc, char** argv)
 {
-    return 0;
+    try
+    {
+        myMain(argc, argv);
+    }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << "[FATAL ERROR]: " << e.what() << '\n';
+        return 9;
+    }
+    catch(const InvalidUsageException& e)
+    {
+        std::cout << "USAGE: " << argv[0] << " [--to-text/--to-nengish] <input_file> <output_file>\n";
+        return 0;
+    }
+}
+
+Options processCommandLineArguments(std::vector<std::string> arguments)
+{
+    Options result = {};
+    
+    if (arguments.size() < 3)
+    {
+        throw InvalidUsageException {};
+    }
+    
+    if (arguments[1] == "--to-text")
+    {
+        result.mode = Mode::NENGISH_TO_TEXT;
+        result.inputFileName = arguments[2];
+        result.outputFileName = arguments[3];
+    } else if (arguments[1] == "--to-nengish")
+    {
+        result.mode = Mode::TEXT_TO_NENGISH;
+        result.inputFileName = arguments[2];
+        result.outputFileName = arguments[3];
+    } else {
+        
+    }
+    
+    return result;
 }
